9fb15cf6d6330412cb2eea43f6d5b9b036054674
azimuth73 <playroger2018@gmail.com>
2024-06-07 23:53:34 +0200

    More refactors for site responsiveness

--- Home.py
+++ Home.py
@@ -1,14 +1,10 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 import random
+from utils import predict_sentiment, download_and_load_model_folders_json, temporarily_download_and_load_model_files
 
 REPOSITORY_LINK = "https://github.com/azimuth73/SimpleSentimentClassificationApp"
 
-MODEL_OPTION_NAMES = ['Model 1', 'Model 2', 'Model 3', 'Model 4']
-
 NEGATIVE_EMOJI_SHORTCODE = ':slightly_frowning_face:'
-NEUTRAL_EMOJI_SHORTCODE = ':neutral_face:'
 POSITIVE_EMOJI_SHORTCODE = ':slightly_smiling_face:'
 
 INPUT_TEXT_AREA_DESC = 'The text that will be used as an input to the selected model for sentiment classification.'
@@ -20,17 +16,48 @@ EVAL_BUTTON_DESC = 'Evaluate the sentiment of the written text using the selecte
 
 st.set_page_config(page_title='Home \u00B7 Simple Sentiment Classification')
 
+if 'is_model_downloading' not in st.session_state:
+    st.session_state.is_model_downloading = False
+
+if 'eval_model_index' not in st.session_state:
+    st.session_state.eval_model_index = 0
+
+if 'model_folders_json' not in st.session_state:
+    st.session_state.model_folders_json = download_and_load_model_folders_json()
+
+if 'model_option_names' not in st.session_state:
+    st.session_state.model_option_names = [model_folder['name'] for model_folder in st.session_state.model_folders_json]
+
+
+@st.cache_resource
+def download_model_files(eval_model_index):
+    if not st.session_state.is_model_downloading:
+
+        st.session_state.is_model_downloading = True
+
+        model_files = temporarily_download_and_load_model_files(
+            st.session_state.model_option_names[eval_model_index]
+        )
+        model, metrics, roc_curve, confusion_matrix = model_files
+        st.session_state.model = model
+        st.session_state.metrics = metrics
+        st.session_state.roc_curve = roc_curve
+        st.session_state.confusion_matrix = confusion_matrix
+
+        st.session_state.is_model_downloading = False
+
+
 if 'eval_button_clicked' not in st.session_state:  # Stateful Button
     st.session_state.eval_button_clicked = False
 
 if 'eval_text' not in st.session_state:
     st.session_state.eval_text = ''
 
-if 'eval_model_index' not in st.session_state:
-    st.session_state.eval_model_index = 0
-
 if 'current_input_text' not in st.session_state:
     st.session_state.current_input_text = ''
+
+if 'model' not in st.session_state:
+    download_model_files(st.session_state.eval_model_index)
 
 
 def eval_button_func(input_text: str, model_name: str) -> None:
@@ -47,14 +74,12 @@ def eval_button_func(input_text: str, model_name: str) -> None:
     st.session_state.eval_model_name = model_name
 
     # TODO: actually implement the chosen model making a prediction
-    st.session_state.eval_score = random.choice([-1, 0, 1])  # Dummy implementation
+    st.session_state.eval_score = random.choice([0, 1])  # Dummy implementation
 
-    if st.session_state.eval_score > 0:
+    if st.session_state.eval_score == 1:
         st.session_state.eval_emoji_shortcode = POSITIVE_EMOJI_SHORTCODE
-    elif st.session_state.eval_score < 0:
+    elif st.session_state.eval_score == 0:
         st.session_state.eval_emoji_shortcode = NEGATIVE_EMOJI_SHORTCODE
-    else:
-        st.session_state.eval_emoji_shortcode = NEUTRAL_EMOJI_SHORTCODE
 
 
 st.text_area(  # Stored in st.session_state.input_text_area
@@ -68,18 +93,19 @@ st.session_state.current_input_text = st.session_state.input_text_area  # Temp t
 
 selected_model_name = st.selectbox(
     label='Select model:',
-    options=MODEL_OPTION_NAMES,
+    options=st.session_state.model_option_names,
     index=st.session_state.eval_model_index,
     key='model_select_box',
-    help=MODEL_SELECT_BOX_DESC
+    help=MODEL_SELECT_BOX_DESC,
 )
-st.session_state.eval_model_index = MODEL_OPTION_NAMES.index(selected_model_name)
+st.session_state.eval_model_index = st.session_state.model_option_names.index(selected_model_name)
+download_model_files(st.session_state.eval_model_index)
 
 eval_func_args = (st.session_state.input_text_area, selected_model_name)  # Can use either temp or input_text_area
 st.button(
     label='Evaluate', key='eval_button', help=EVAL_BUTTON_DESC,
     on_click=eval_button_func, args=eval_func_args, kwargs=None,
-    type="secondary", use_container_width=True
+    type='secondary', use_container_width=True
 )
 
 # TODO: Make proper output based on the prediction of the chosen model
---
+++ utils.py
@@ -0,0 +1,133 @@
+import torch
+from torch import nn
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import os
+from dotenv import load_dotenv
+import gdown
+import json
+import shutil
+import numpy as np
+from PIL import Image
+
+
+class TransformerBinarySequenceClassificator(nn.Module):
+    def __init__(self, model_name: str, requires_grad: bool = True) -> None:
+        super(TransformerBinarySequenceClassificator, self).__init__()
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, do_lower_case=True)
+        self.requires_grad = requires_grad
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+        for parameter in self.model.parameters():
+            parameter.requires_grad = requires_grad
+
+    def forward(self, batch_sequences, batch_sequence_masks, batch_sequence_segments, labels=None):
+        if labels is not None:
+            outputs = self.model(
+                input_ids=batch_sequences,
+                attention_mask=batch_sequence_masks,
+                token_type_ids=batch_sequence_segments,
+                labels=labels
+            )
+            loss, logits = outputs.loss, outputs.logits
+        else:
+            outputs = self.model(
+                input_ids=batch_sequences,
+                attention_mask=batch_sequence_masks,
+                token_type_ids=batch_sequence_segments
+            )
+            loss = None
+            logits = outputs.logits
+        probabilities = nn.functional.softmax(logits, dim=-1)
+        return loss, logits, probabilities
+
+
+def predict_sentiment(model, sentence: str, max_sequence_length: int = 50):
+    # Process the sentence
+    tokens_sequence = model.tokenizer.tokenize(sentence)
+    tokens_sequence = ['[CLS]'] + tokens_sequence
+    if len(tokens_sequence) > max_sequence_length:
+        tokens_sequence = tokens_sequence[:max_sequence_length]
+    padding = [0] * (max_sequence_length - len(tokens_sequence))
+    input_ids = model.tokenizer.convert_tokens_to_ids(tokens_sequence)
+    input_ids += padding
+    attention_mask = [1] * len(tokens_sequence) + padding
+    token_type_ids = [0] * max_sequence_length
+
+    # Convert to tensors
+    input_ids = torch.tensor([input_ids]).type(torch.long)
+    attention_mask = torch.tensor([attention_mask]).type(torch.long)
+    token_type_ids = torch.tensor([token_type_ids]).type(torch.long)
+
+    # Make prediction
+    with torch.no_grad():
+        _, _, probabilities = model(input_ids, attention_mask, token_type_ids)
+    prediction = torch.argmax(probabilities, dim=1).item()
+
+    return prediction
+
+
+def download_and_load_model_folders_json():
+    load_dotenv()
+
+    model_folders_json_id = os.getenv('MODEL_FOLDERS_JSON_ID')
+    gdown.download(id=model_folders_json_id, output='model_folders.json')
+
+    with open('model_folders.json', 'r') as f:
+        model_folders_json = json.load(f)
+
+    return model_folders_json
+
+
+def temporarily_download_and_load_model_files(model_name: str):
+    def load_model(name: str, filepath: str):
+        classificator = TransformerBinarySequenceClassificator(model_name=name, requires_grad=False)
+        classificator.load_state_dict(torch.load(filepath, map_location=torch.device('cpu')))
+        classificator.eval()
+        return classificator
+
+    def load_metrics(filepath):
+        data: dict = {}
+        with open(filepath, 'r') as file:
+            for line in file:
+                metric_name, metric_value = line.strip().split('\t')
+                data[metric_name] = float(metric_value)
+        return data
+
+    def load_image(filepath):
+        image = Image.open(filepath)
+        image = image.convert('RGB')
+        image = np.array(image)
+
+        return image
+
+    model_folders_json = download_and_load_model_folders_json()
+    model_folder_id = None
+    for model_folder_json in model_folders_json:
+        if model_folder_json['name'] == model_name:
+            model_folder_id = model_folder_json['id']
+            break
+    if not model_folder_id:
+        return None
+
+    download_path = f'models/{model_name}'
+    os.makedirs(download_path, exist_ok=True)
+    gdown.download_folder(id=model_folder_id, output=download_path, quiet=False)
+
+    model = load_model(model_name, os.path.join(download_path, 'model.pt'))
+    metrics: dict = load_metrics(os.path.join(download_path, 'metrics.tsv'))
+    roc_curve_image = load_image(os.path.join(download_path, 'roc_curve.png'))
+    confusion_matrix_image = load_image(os.path.join(download_path, 'confusion_matrix.png'))
+
+    # Delete the downloaded model folder
+    shutil.rmtree(download_path)
+
+    return model, metrics, roc_curve_image, confusion_matrix_image
+
+
+def main():
+    download_and_load_model_folders_json()
+
+
+if __name__ == '__main__':
+    main()
